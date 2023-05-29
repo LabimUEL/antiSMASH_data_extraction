236f11ac86e7bf843f49b832a6e0a73a3cbb1997
brenoslivio <brenoslivio@usp.br>
2023-05-29 14:32:27 -0300

    Path typo.

--- accessions.py
+++ accessions.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 acc_antifungi = pd.read_csv('data/antifungi/all_features_CDS_motifs.csv')['acc']
-acc_nonantifungi = pd.read_csv('data/antifungi/all_features_CDS_motifs.csv')['acc']
+acc_non_antifungi = pd.read_csv('data/non_antifungi/all_features_CDS_motifs.csv')['acc']
 
-accessions = pd.concat([acc_antifungi, acc_nonantifungi], axis = 0)
+accessions = pd.concat([acc_antifungi, acc_non_antifungi], axis = 0)
 
 print(accessions.unique())
